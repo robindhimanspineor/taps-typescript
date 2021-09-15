e8105aa644d5d5c56a4022d8069c039f504c2f5c
Robin <mohit@ebctechnologies.com>
2021-09-15 15:40:36 +0530

    commented

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -5,22 +5,22 @@ import CART_QUERY from '../../queries/cartQuery';
 import styles from '../../styles/Navbar.module.css';
 
 const Navbar = ({ items }: any) => {
-  const { data, refetch } = useQuery(CART_QUERY, {
-    variables: {
-      cart_id: getCookie('cart_id'),
-      operation: 'view_cart',
-      user_id: getCookie('taps_uuid'),
-      zip_code: '12345',
-    },
-  });
+  // const { data, refetch } = useQuery(CART_QUERY, {
+  //   variables: {
+  //     cart_id: getCookie('cart_id'),
+  //     operation: 'view_cart',
+  //     user_id: getCookie('taps_uuid'),
+  //     zip_code: '12345',
+  //   },
+  // });
 
   return (
     <nav className={styles.navbar}>
       Auto Parts
       <div className={styles.rightContainer}>
-        {data?.store?.outerCart?.cart?.totalNumberOfItems
+        {/* {data?.store?.outerCart?.cart?.totalNumberOfItems
           ? data?.store?.outerCart?.cart?.totalNumberOfItems
-          : ''}{' '}
+          : ''}{' '} */}
         Items
       </div>
     </nav>
